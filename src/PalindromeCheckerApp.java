5e16499f97389981c4ed198d928acb4eaf9d8844
bsupriyayadav <bsupriyayadav@gmail.com>
2026-02-19 10:59:01 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -6,5 +6,19 @@ public class PalindromeCheckerApp {
         System.out.println("Version 1.0");
         System.out.println("UC1: Application Entry & Welcome Message");
 
+        // ======================
+        // UC2: Hardcoded Palindrome Check
+        // ======================
+
+        System.out.println("\nUC2: Hardcoded Palindrome Check");
+
+        String word = "madam";
+
+        if (word.equals(new StringBuilder(word).reverse().toString())) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+
     }
 }
